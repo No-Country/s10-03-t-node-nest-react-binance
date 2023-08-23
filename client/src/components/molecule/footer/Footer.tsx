0df86cd19c25e69a424ae6eae8ca968e19467b3a
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-08-23 00:08:48 -0300

    #feat/header-footer -empiezo con el footer

--- client/src/components/molecule/footer/Footer.tsx
+++ client/src/components/molecule/footer/Footer.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { Grid, Typography } from "@mui/material"
+import { FOOTER_STYLES } from './FooterStyles'
+import NavBar from '../navbar/NavBar'
 
 interface FooterProps { }
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <Grid
+        container
+        maxWidth="lg"
+        sx={ FOOTER_STYLES.container }
+      >
+        <Grid
+          item
+          xs={ 12 }
+          sx={ FOOTER_STYLES.gridYear }
+        >
+          <Typography>
+            Binance &copy; { new Date().getFullYear() }
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={ 12 }
+          sx={ FOOTER_STYLES.gridNavBar }
+        >
+          <Typography>
+            <NavBar />
+          </Typography>
+        </Grid>
+      </Grid>
+    </footer>
   )
 }
 

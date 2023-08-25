7c02016245051ac7e47a9ac98d35252c27e59cf5
SergioCalbino <serggio.c86@gmail.com>
2023-08-25 17:15:45 -0300

    Validaciones del register ok

--- client/src/components/template/personal-account/PersonalAccount.tsx
+++ client/src/components/template/personal-account/PersonalAccount.tsx
@@ -1,18 +1,80 @@
-import * as React from 'react'
+import * as React from 'react';
+import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom'
-import { Typography, Container, Box, TextField } from '@mui/material'
+import { Typography, Container, Box, TextField, Alert, AlertTitle,Snackbar } from '@mui/material'
 import PrimaryButton from '../../atom/buttons/PrimaryButton'
 import { PERSONAL_STYLES } from './PersonalAccountStyles'
 
 interface PersonalAccountProps { }
 
 const PersonalAccount: React.FC<PersonalAccountProps> = () => {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState({text: '', msg: ''});
+  const [welcomeMessage, setWelcomeMessage] = useState({text: ''})
+  const [showMessage, setShowMessage] = useState(false)
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const isValidEmail = emailRegex.test(email);
+
   const navigate = useNavigate()
-  const handleContinue = () => {
+  
+  const handleRegister = async() => {
     // TODO: hay que hacer bien lo de crear la cuenta personal
     // TODO: antes del navigate que salga algun cartel avisando el ok o no
-    navigate('/market')
-  }
+    if (!password || password.length < 6) {
+      setError(true);
+      setMessage({
+        text: 'El password es obligatorio y debe tener mas de 6 caracteres',
+        msg: 'password invalido'
+      })
+      setTimeout(() => {
+        setError(false)
+      }, 3000);
+    return
+    }
+    setWelcomeMessage({
+      text: 'Bienvenido'
+    });
+    setShowMessage(true)
+    setTimeout(() => {
+      navigate('/market')
+      
+    }, 3000);
+    
+  };
+
+  const handleNextClick = () => {
+    if ([email].includes('')) {
+      setError(true)
+      setMessage({
+        text: 'El email es obligatorio',
+        msg: 'Email'
+      })
+      setTimeout(() => {
+        setError(false)
+      }, 3000);
+    return
+    }
+    if (!isValidEmail) {
+      setError(true)
+      setMessage({
+        text: 'El email contiene caracteres invalidos',
+        msg: 'Email invalido'
+      })
+      setTimeout(() => {
+        setError(false)
+      }, 3000);
+    return
+    }
+    setShowPassword(true)
+   
+  };
+
+ 
 
   return (
     <main style={ PERSONAL_STYLES.main }>
@@ -30,7 +92,40 @@ const PersonalAccount: React.FC<PersonalAccountProps> = () => {
               id="filled-basic"
               label="Correo / Teléfono"
               variant="filled"
-              style={ { borderRadius: 0, width: '70%', marginBottom: '20px' } } />
+              style={ { borderRadius: 0, width: '70%', marginBottom: '20px' } } 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            {
+              error &&
+              <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                {message.text} — <strong>{message.msg}</strong>
+              </Alert>
+             }
+             
+             {
+              showPassword && (
+                <TextField
+                id="filled-basic"
+                label="password"
+                variant="filled"
+                style={ { borderRadius: 0, width: '70%', marginBottom: '20px' } } 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+              )
+            }
+
+          {
+              showMessage && 
+                <Alert severity="success">
+                  <AlertTitle>Success</AlertTitle>
+                {welcomeMessage.text} — <strong>Registro con Exito! Seras redireccionado al Market</strong>
+                </Alert>
+          }
+
             <Typography
               variant="body1"
               my={ 2 }
@@ -49,25 +144,15 @@ const PersonalAccount: React.FC<PersonalAccountProps> = () => {
               </Box>
             </Typography>
             <PrimaryButton
-              text='Continuar'
+              text={!showPassword ? 'Siguiente' : 'Registrarse'}
               ariaLabelText='Continuar'
               variant='contained'
               color='primary'
-              onClick={ handleContinue }
+              onClick={ ( showPassword ? handleRegister : handleNextClick) }
             />
+            
           </form>
-          <Typography my={ 2 }>
-            Si no quieres una cuenta personal,
-            <Link
-              to={ '/register/continue' }
-              style={ PERSONAL_STYLES.link }
-              aria-label='inscribirse en cuenta entidad'
-            >
-              <Box component="span">
-                inscríbete en una cuenta de entidad
-              </Box>
-            </Link>
-          </Typography>
+          
         </Box>
       </Container>
     </main>

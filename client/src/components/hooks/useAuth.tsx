5480f299450e05f4a5a0ec2c218c203ba14af1f8
SergioCalbino <serggio.c86@gmail.com>
2023-08-26 13:31:57 -0300

    Contexto Auth Solucionado

--- client/src/components/hooks/useAuth.tsx
+++ client/src/components/hooks/useAuth.tsx
@@ -1,8 +1,20 @@
-import { useContext } from "react"
-import AuthContext from "../../context/AuthContext"
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
 
-const useAuth = () => {
-    return useContext(AuthContext)
+interface AuthMethods {
+    registerAuth: (email: string, password: string) => void;
 }
 
-export default useAuth
+const useAuth = (): AuthMethods | undefined => {
+    const authContext = useContext(AuthContext);
+
+    if (!authContext) {
+        return undefined; // Retornar undefined cuando el contexto no está definido
+    }
+
+    return {
+        registerAuth: authContext.registerAuth
+    };
+};
+
+export default useAuth;

8103c69ea3f08a278c6d00bedd43a54413a7a37b
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-09-06 20:24:45 -0300

    -comienzo con la pagina not found

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Button, Grid, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 interface NotFoundProps { }
 
 const NotFound: React.FC<NotFoundProps> = () => {
+  const navigate = useNavigate()
+
   return (
-    <>Not Found</>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={ 4 }
+      maxWidth="lg"
+    >
+      <Grid item xs={ 12 }>
+        <Typography
+          variant="h2"
+        >
+          Ups! No encontramos la página buscada.
+        </Typography>
+      </Grid>
+      <Grid item xs={ 12 }>
+        <Button
+          onClick={ () => navigate('/market') }
+          size='small'
+          aria-label="volver a mercado"
+          variant="contained"
+          color="primary"
+        >
+          Volver a mercado
+        </Button>
+      </Grid>
+    </Grid>
   )
 }
 

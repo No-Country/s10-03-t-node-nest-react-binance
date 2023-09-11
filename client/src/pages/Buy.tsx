3f079847d69e3ea13d3447132587b92757e18808
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-09-11 12:26:48 -0300

    -ajustes

--- client/src/pages/Buy.tsx
+++ client/src/pages/Buy.tsx
@@ -1,34 +1,38 @@
 import React from 'react'
 import { Container, Typography } from '@mui/material'
 import SearchBar from '../components/molecule/searchBar'
-import TabsSales from '../components/molecule/tabs/TabsSales'
+import CoinsSellBuyTable from '../components/molecule/coins-table/CoinsSellBuyTable'
+import { useLocation } from 'react-router-dom'
+import { useApiContext } from '../context/FetchContext'
 
 const Buy = () => {
+  const { coinsData } = useApiContext()
+  const location = useLocation()
+  const urlPathName = location.pathname
+
   return (
-    <Container maxWidth="xs">
+    <Container maxWidth="sm" sx={{ minHeight: '82vh'}}>
       <Typography
-        variant="h4"
+        variant="h2"
         align="center"
-        sx={ {
-          marginTop: '55px',
-          marginBottom: '20px'
-        } }
+        sx={ { marginTop: '55px', marginBottom: '20px' } }
       >
         Elegir activo a comprar
       </Typography>
       <SearchBar />
       <Typography
-        variant="h4"
+        variant="h3"
         align="left"
-        sx={ {
-          color: 'red',
-          marginBottom: '50px'
-        } }
+        sx={ { color: 'red', marginBottom: '50px' } }
       >
         Tenencias en cartera
       </Typography>
-      <TabsSales />
-    </Container>  
+      <CoinsSellBuyTable
+        urlPathName={ urlPathName }
+        btnText="Comprar"
+        coinsData={ coinsData }
+      />
+    </Container>
   )
 }
 

c7b87ad9a75abd11ea47abc8761c5dca5f0b5834
Edu568 <eduardo.arevalo072@gmail.com>
2023-09-11 21:28:11 -0300

    se traen cambios de development donde se solucionaron los errores en ruta buy/scree y sell/screen

--- client/src/protectedRoutes/index.tsx
+++ client/src/protectedRoutes/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Navigate } from "react-router-dom"
-import { useAuth } from "../context/AuthContext"
+// import React from "react";
+// import { Navigate } from "react-router-dom"
+// import { useAuth } from "../context/AuthContext"
 
-export default function protectedRoutes({children}) {
-    const {email} = useAuth()
-    if(!email) return <Navigate to='/login'/>
-    return <>{children}</>
-}
+// export default function protectedRoutes({children}) {
+//     const {email} = useAuth()
+//     if(!email) return <Navigate to='/login'/>
+//     return <>{children}</>
+// }
 

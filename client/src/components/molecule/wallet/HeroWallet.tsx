6bcaec5946e6bb41b3ab24817a35754159a709c1
vic-ferr <victorhfp2014@gmail.com>
2023-09-13 21:32:14 -0300

    fix hero y wallet

--- client/src/components/molecule/wallet/HeroWallet.tsx
+++ client/src/components/molecule/wallet/HeroWallet.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Box, Grid, Typography } from '@mui/material'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 import VisibilityIcon from '@mui/icons-material/Visibility'
 import { HERO_MARKET_STYLES } from '../../template/hero-market/HeroMarketStyles'
-
+import AuthContext from '../../../context/AuthContext'
 const HeroWallet = () => {
     const [isAmountVisible, setIsAmountVisible] = useState(true)
+    const { loginAuth } = useContext(AuthContext);
+    console.log(loginAuth);
 
     const handleVisibilityToggle = () => {
         setIsAmountVisible((prevIsAmountVisible) => !prevIsAmountVisible)
@@ -15,39 +17,39 @@ const HeroWallet = () => {
         <>
             <Grid
                 item
-                xs={ 12 }
-                sm={ 4 }
-                md={ 3 }
-                sx={ HERO_MARKET_STYLES.container }
+                xs={12}
+                sm={4}
+                md={3}
+                sx={HERO_MARKET_STYLES.container}
             >
                 <Grid
                     container
-                    sx={ HERO_MARKET_STYLES.containerSaldo }
+                    sx={HERO_MARKET_STYLES.containerSaldo}
                 >
                     <Grid
                         item
-                        xs={ 12 }
-                        sx={ HERO_MARKET_STYLES.saldoText }
+                        xs={12}
+                        sx={HERO_MARKET_STYLES.saldoText}
                     >
                         <Typography
                             component="h2"
-                            sx={ { fontSize: '24px', fontWeight: '700' } }
+                            sx={{ fontSize: '24px', fontWeight: '700' }}
                         >
                             Saldo total
                         </Typography>
                         <Box
                             component="span"
-                            onClick={ handleVisibilityToggle }
+                            onClick={handleVisibilityToggle}
                         >
-                            { isAmountVisible ? <VisibilityOffIcon /> : <VisibilityIcon /> }
+                            {isAmountVisible ? <VisibilityOffIcon /> : <VisibilityIcon />}
                         </Box>
                     </Grid>
-                    <Grid item xs={ 12 }>
+                    <Grid item xs={12}>
                         <Typography
                             component="h2"
-                            sx={ HERO_MARKET_STYLES.saldoAmount }
+                            sx={HERO_MARKET_STYLES.saldoAmount}
                         >
-                            { isAmountVisible ? '$100.00' : '******' }
+                            {isAmountVisible ? `${loginAuth.balance}` : '******'}
                         </Typography>
                     </Grid>
                 </Grid>
